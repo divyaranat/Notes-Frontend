a98559dcf5b0bf6a26ba51d1667e741bfc4246b3
Divya Ranat <38928132+divyaranat@users.noreply.github.com>
2023-10-05 10:35:00 -0400

    Revert render code

--- public/react/index.js
+++ public/react/index.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from 'react-dom/client';
 import {App} from './components/App';
 
-// ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+// const root = createRoot(document.getElementById("root"));
+// root.render(<App />);
